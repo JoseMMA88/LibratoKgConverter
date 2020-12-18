5b6b277326353f71e97f425c9d8cca5549487a96
JoseMMA88 <josemma_97@hotmail.com>
2020-12-18 16:12:31 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+from tkinter import *
+
+
+def convert():
+    label_4.config(text=str(float(input.get()) * 0.453592))
+
+
+# Window Setup
+window = Tk()
+window.title("Libra to Kg Converter")
+window.minsize(width=500, height=500)
+
+
+# Setup labels
+label = Label(text="Libras")
+label.grid(column=2, row=0)
+
+label_2 = Label(text="Kgs")
+label_2.grid(column=2, row=1)
+
+label_3 = Label(text="is equal to")
+label_3.grid(column=0, row=1)
+
+label_4 = Label(text="0")
+label_4.grid(column=1, row=1)
+
+# Setup Textinput
+input = Entry(width=10)
+input.grid(column=1, row=0)
+
+# Setup Button
+button = Button(text="Calculate", command=convert)
+button.grid(column=1, row=3)
+
+# Window mainloop
+window.mainloop()
